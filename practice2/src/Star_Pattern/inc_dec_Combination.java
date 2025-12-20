95d72bab3fefd8bbb935bfab597402150ba7bb6a
Shailesh <shaileshutkar77@gmail.com>
2025-12-20 14:45:50 +0530

    updation stars

--- practice2/src/Star_Pattern/inc_dec_Combination.java
+++ practice2/src/Star_Pattern/inc_dec_Combination.java
@@ -8,7 +8,7 @@ public class inc_dec_Combination {
 			}
 			System.out.println();
 		}
-		for (int i = 1; i <= 4; i++) {
+		for (int i = 1; i <= 3; i++) {
 			for (int j = 3; j >= i; j--) {
 				System.out.print("*");
 			}
