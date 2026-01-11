833d3794624e471fe5790fe5b982b3fda3c5f844
Shailesh <shaileshutkar77@gmail.com>
2026-01-11 16:52:21 +0530

    piramid3 added

---
+++ practice2/src/Star_Pattern/Priamid3.java
@@ -0,0 +1,18 @@
+package Star_Pattern;
+
+public class Priamid3 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 4; k >= i; k--) {
+				System.out.print("*");
+			}
+			for (int l = 4; l > i; l--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
