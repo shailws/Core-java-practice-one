116cee6170cb740035589739e820a8b056558ba0
Shailesh <shaileshutkar77@gmail.com>
2026-01-16 13:53:45 +0530

    forword and inner stars pattern

---
+++ practice2/src/Star_Pattern/Forwared_stars.java
@@ -0,0 +1,16 @@
+package Star_Pattern;
+
+public class Forwared_stars {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (i >= 2 & j <= i - 1) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
