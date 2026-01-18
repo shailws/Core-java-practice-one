d577be089219a7db566e83e37a116af325481448
Shailesh <shaileshutkar77@gmail.com>
2026-01-18 14:21:50 +0530

    outline star  patter

---
+++ practice2/src/Star_Pattern/Outline.java
@@ -0,0 +1,16 @@
+package Star_Pattern;
+
+public class Outline {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				if (i >= 2 & j >= 2 & j <= 3 & i <= 3) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
