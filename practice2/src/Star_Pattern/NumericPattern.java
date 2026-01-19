5c62e2cc6472f68e52d4fe16c702e749d74fcaa1
Shailesh <shaileshutkar77@gmail.com>
2026-01-19 15:08:54 +0530

    numeric pattern added

---
+++ practice2/src/Star_Pattern/NumericPattern.java
@@ -0,0 +1,13 @@
+package Star_Pattern;
+
+public class NumericPattern {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+//				System.out.print(j);
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
