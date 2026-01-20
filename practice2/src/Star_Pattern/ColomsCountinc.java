9be1173ebc383a26576a4e72782291ad91444842
Shailesh <shaileshutkar77@gmail.com>
2026-01-20 14:06:04 +0530

    count increasing

---
+++ practice2/src/Star_Pattern/ColomsCountinc.java
@@ -0,0 +1,15 @@
+package Star_Pattern;
+
+public class ColomsCountinc {
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= i; j++) {
+				 count++;
+				System.out.print(count);
+			}
+			System.out.println();
+		}
+
+	}
+}
